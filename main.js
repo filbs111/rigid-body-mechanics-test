bd2f9e2fd2bdb9bbdbc0d8840245ae6ef6c32edb
Firstname Lastname <filbs111@protonmail.com>
2024-11-29 00:44:20 +0000

    circle-circle collision

--- main.js
+++ main.js
@@ -53,17 +53,116 @@ addPhysicsObject({
     invDensity: 0,
     fillStyle: "black"
 });
+
+//CIRCLES
 addPhysicsObject({
-    position: [350,300],
+    position: [400,50],
     velocity: [-1,0],
     objType: "circle",
     radius: 40,
     cor: 0.6,
     invDensity: 1,
-    fillStyle: "blue"
+    fillStyle: "purple"
+});
+addPhysicsObject({
+    position: [200,50],
+    velocity: [1,0],
+    objType: "circle",
+    radius: 50,
+    cor: 0.6,
+    invDensity: 1,
+    fillStyle: "cyan"
+});
+addPhysicsObject({
+    position: [200,50],
+    velocity: [0,-0.5],
+    objType: "circle",
+    radius: 20,
+    cor: 0.6,
+    invDensity: 1,
+    fillStyle: "orange"
 });
 
+function effectiveCor(object1, object2){
+    return Math.min(object1.cor, object2.cor); //TODO does this make sense?
+}
+
+//NOTE could generalise circles and rectangles to rounded boxes
+function processPossibleCollisionCircleCircle(object1, object2){
+
+    //same as rectangle collision. TODO generalise this?
+
+    var positionDifference = [
+        object1.position[0] - object2.position[0],
+        object1.position[1] - object2.position[1]
+    ];
+    var velocityDifference = [
+        object1.velocity[0] - object2.velocity[0],
+        object1.velocity[1] - object2.velocity[1]
+    ];
+
+    var separationSq = positionDifference[0]*positionDifference[0] + positionDifference[1]*positionDifference[1];
+    var totalRad = object1.radius + object2.radius;
+    var touchingDistanceSq = totalRad * totalRad;
+
+    if (separationSq < touchingDistanceSq){
+        //colliding
+        var cor = effectiveCor(object1, object2);
+
+        var displacementDotVelocityDifference = positionDifference[0]*velocityDifference[0] + positionDifference[1]*velocityDifference[1]
+
+        if (displacementDotVelocityDifference<0){
+            //approaching
+
+            //TODO reflect velocity in this direction, retain other component
+            var cOfMVelocity = [
+                (object1.velocity[0]*object2.invMass + object2.velocity[0]*object1.invMass)/(object1.invMass+ object2.invMass),
+                (object1.velocity[1]*object2.invMass + object2.velocity[1]*object1.invMass)/(object1.invMass+ object2.invMass)
+                ];
+                //^^ is this needed? or just velocity along separation?
+
+                //for each object, subtract centre of mass velocity to get velocity in frame travelling at centre of mass velocity
+                //in this frame, for each object, retain component perpendicular to displacement (=reaction force) direction
+                //and multiply the other part by negative coefficient of resitution.
+                //add the centre of mass velocity back.
+                //TODO reformulate for efficiency? (removal of square roots etc)
+
+            updateSpeedForObject(object1);
+            updateSpeedForObject(object2);
+
+            function updateSpeedForObject(theObject){
+                var velInMovingFrame = [
+                    theObject.velocity[0]- cOfMVelocity[0],
+                    theObject.velocity[1]- cOfMVelocity[1]
+                ];
+    
+                var mulltiplier1 = (velInMovingFrame[0]*positionDifference[0] + velInMovingFrame[1]*positionDifference[1])
+                        /separationSq;
+    
+                var velInMovingFrameComponentAlongReactionNormal = 
+                    [positionDifference[0]*mulltiplier1, positionDifference[1]*mulltiplier1];
+    
+                var perpendicularPart = [
+                    velInMovingFrame[0] - velInMovingFrameComponentAlongReactionNormal[0],
+                    velInMovingFrame[1] - velInMovingFrameComponentAlongReactionNormal[1],
+                ];
+    
+                //TODO formulate using impulses instead?
+                theObject.velocity = [
+                    cOfMVelocity[0] + perpendicularPart[0] - cor * velInMovingFrameComponentAlongReactionNormal[0],
+                    cOfMVelocity[1] + perpendicularPart[1] - cor * velInMovingFrameComponentAlongReactionNormal[1],
+                ];
+            }
+            
+        }
+    }
+}
+
 function processPossibleCollision(object1, object2){
+
+    if ( (object1.objType == "circle") && (object2.objType == "circle")){
+        return processPossibleCollisionCircleCircle(object1, object2);
+    }
 
     //temp -no collision for circles yet
     if ( (object1.objType == "circle") || (object2.objType == "circle")){
@@ -94,21 +193,21 @@ function processPossibleCollision(object1, object2){
 
     if (separation[0]<0 && separation[1]<0){
         //is colliding
-        var effectiveCorR = Math.min(object1.cor, object2.cor); //TODO does this make sense?
+        var cor = effectiveCor(object1, object2);
 
         if (separation[0]>separation[1]){
             //momentum = m1v1 + m1v2. centre of mass speed = (m1v1+m2v2)/(m1+m2)
             // = (v1/m2 + v2/m1)/(1/m1 + 1/m2)
             if (positionDifference[0]*velocityDifference[0]<0){
                 var cOfMSpeed = (object1.velocity[0]*object2.invMass + object2.velocity[0]*object1.invMass)/(object1.invMass+ object2.invMass);
-                object1.velocity[0] = (1+effectiveCorR)* cOfMSpeed - effectiveCorR*object1.velocity[0];
-                object2.velocity[0] = (1+effectiveCorR)* cOfMSpeed - effectiveCorR*object2.velocity[0];
+                object1.velocity[0] = (1+cor)* cOfMSpeed - cor*object1.velocity[0];
+                object2.velocity[0] = (1+cor)* cOfMSpeed - cor*object2.velocity[0];
             }
         }else{
             if (positionDifference[1]*velocityDifference[1]<0){
                 var cOfMSpeed = (object1.velocity[1]*object2.invMass + object2.velocity[1]*object1.invMass)/(object1.invMass+ object2.invMass);
-                object1.velocity[1] = (1+effectiveCorR)* cOfMSpeed - effectiveCorR*object1.velocity[1];
-                object2.velocity[1] = (1+effectiveCorR)* cOfMSpeed - effectiveCorR*object2.velocity[1];
+                object1.velocity[1] = (1+cor)* cOfMSpeed - cor*object1.velocity[1];
+                object2.velocity[1] = (1+cor)* cOfMSpeed - cor*object2.velocity[1];
             }
         }
 
